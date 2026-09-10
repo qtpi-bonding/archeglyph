8d09b5e7c4f9cdebc7cde1e51f76f69d95f47eaf
qtpi-bonding <qtpi-bonding@noreply.email.com>
2026-09-10 15:55:34 -0700

    feat: packages/core/src/geometry/vec2/impl.ts/add().

--- packages/core/src/geometry/vec2/impl.ts
+++ packages/core/src/geometry/vec2/impl.ts
@@ -10,7 +10,10 @@ export function length(v: Vec2): number {
   throw new Error('not implemented');
 }
 export function add(a: Vec2, b: Vec2): Vec2 {
-  throw new Error('not implemented');
+  return {
+    x: a.x + b.x,
+    y: a.y + b.y,
+  };
 }
 export function distance(a: Vec2, b: Vec2): number {
   throw new Error('not implemented');

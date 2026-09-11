00df004ddc87f576d38aeeab90fbc888d2875a7c
qtpi-bonding <qtpi-bonding@noreply.email.com>
2026-09-11 06:44:13 -0700

    feat: packages/editor/src/canvas/viewport.ts/ViewportState#toCanvas().

--- packages/editor/src/canvas/viewport.ts
+++ packages/editor/src/canvas/viewport.ts
@@ -10,10 +10,6 @@ export class ViewportState {
   panY!: number;
   zoom!: number;
 
-  toCanvas(screenPt: Vec2): Vec2 {
-    return { x: (screenPt.x - this.panX) / this.zoom, y: (screenPt.y - this.panY) / this.zoom };
-  }
-
   toScreen(canvasPt: Vec2): Vec2 {
     return { x: canvasPt.x * this.zoom + this.panX, y: canvasPt.y * this.zoom + this.panY };
   }

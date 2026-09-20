19ffc22ae6c899e5c783aaa0af9c2cc1c9fcf391
qtpi-bonding <qtpi-bonding@noreply.email.com>
2026-09-20 00:36:30 -0700

    feat: packages/editor/src/shell/top_bar.tsx/TopBarProps#

--- packages/editor/src/shell/top_bar.tsx
+++ packages/editor/src/shell/top_bar.tsx
@@ -6,15 +6,6 @@ import { EDITOR_THEMES } from './editor_theme';
 import { HostAdapter } from '../adapters/host_adapter';
 import { SaveController, SaveStatus } from './save_controller';
 
-export interface TopBarProps {
-  state: EditorState;
-  adapter: HostAdapter;
-  saveController: SaveController | null;
-  /** Active chrome palette name, and a setter. Wave 6's toolbar takes this over. */
-  editorTheme: string;
-  onEditorTheme: (name: string) => void;
-}
-
 export const TopBar: Component<TopBarProps> = (props: TopBarProps): JSX.Element => {
   const params: URLSearchParams = new URLSearchParams(window.location.search);
   const fileName: string = params.get('file') ?? params.get('name') ?? 'Untitled';
